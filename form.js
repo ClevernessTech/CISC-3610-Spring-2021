c2577efe70bab6c95219e923abd112c32844b6b3
Jose Soto <cleverness@pop-os.localdomain>
2021-03-14 20:34:25 -0400

    Uploading Form lab

---
+++ form.js
@@ -0,0 +1,28 @@
+var clear_btn = document.getElementById("btn-clear");
+var notes = document.getElementById("notes");
+var title = document.getElementById("title");
+
+//Main function that handles saving data to local storage.  Only have title and notes field, so 
+function saveData(){
+    localStorage.setitem("title", title.value);
+    localStorage.setItem("notes", notes.value);
+}
+
+//Per lab instructions, oninput will call saveData function when notes field is interacted with to save to local storage.
+notes.oninput = function(){
+    saveData();
+}
+
+//uses getItem command of localStorage to fill the 2 fields when the window loads
+window.onload = function(){
+    title.value = localStorage.getItem("title");
+    notes.value = localStorage.getItem("notes");
+}
+
+//Couldn't use localStorage.clear here since it would reset all pairs, so just emptied out the notes and titles of any chars/string values
+clear_btn.onclick = function() {
+    title.value = "";
+    notes.value = "";
+    saveData();
+}
+
